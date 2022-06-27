3049b68676571781bb923e3ac89eda92f0717bc3
EkaterinaKhliabets <e.v.khliabets@gmail.com>
2022-06-27 12:49:12 +0300

    main page

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return 111;
-});*/
+Route::get('/', function () {
+    return 'Hello,user';
+});
 
-Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+//Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
 
 
